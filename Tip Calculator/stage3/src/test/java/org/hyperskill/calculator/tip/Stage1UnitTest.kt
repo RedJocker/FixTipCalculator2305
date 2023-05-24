1318f536aa16dc16e77a5a82e09fcd0ee8428891
RedJocker <mauricio.guaruja@gmail.com>
2023-05-24 17:41:44 -0300

    new reference solution and tests

--- Tip Calculator/stage3/src/test/java/org/hyperskill/calculator/tip/Stage1UnitTest.kt
+++ Tip Calculator/stage3/src/test/java/org/hyperskill/calculator/tip/Stage1UnitTest.kt
@@ -1,35 +1,60 @@
 package org.hyperskill.calculator.tip
 
 import org.hyperskill.calculator.tip.internals.TipCalculatorUnitTest
+import org.hyperskill.calculator.tip.internals.screen.TipCalculatorScreen
 import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
 import org.robolectric.RobolectricTestRunner
 
-// Version 2.0
+// Version 3.0
 @RunWith(RobolectricTestRunner::class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class Stage1UnitTest : TipCalculatorUnitTest<MainActivity>(MainActivity::class.java) {
 
     @Test
     fun test00_checkEditText() {
-         testActivity {
-            editText
+        testActivity {
+            TipCalculatorScreen(this, initViews = false).apply {
+                editText
+            }
         }
     }
 
     @Test
     fun test01_checkSlider() {
         testActivity {
-            slider
+            TipCalculatorScreen(this, initViews = false).apply {
+                seekBar
+            }
         }
     }
 
     @Test
-    fun test02_checkTextView() {
+    fun test02_checkBillValueTextView() {
         testActivity {
-            textView
+            TipCalculatorScreen(this, initViews = false).apply {
+                billValueTextView
+            }
+        }
+    }
+
+    @Test
+    fun test03_checkTipPercentTextView() {
+        testActivity {
+            TipCalculatorScreen(this, initViews = false).apply {
+                tipPercentTextView
+            }
+        }
+    }
+
+    @Test
+    fun test04_checkTipAmountTextView() {
+        testActivity {
+            TipCalculatorScreen(this, initViews = false).apply {
+                tipAmountTextView
+            }
         }
     }
 }
